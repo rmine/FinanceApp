90bc3b1f19ff4e1d2269ca45630c4b6a30e8aaac
rmine <huangzr.kai7@gmail.com>
2015-08-11 15:33:20 +0800

    update routing config

--- config/routes.rb
+++ config/routes.rb
@@ -1,64 +1,4 @@
 FinanceApp::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-
-  #root :to=> "home#index"
-  get 'api', :to=>'api#index'
-
-  match 'api/register_user', :to=>'api#register_user', via: [:get,:post]
-
-  match 'api/register', :to=>'api#register',via: [:get,:post]
-
-  match 'api/login', :to=>'api#login',via: [:get,:post]
-
-  match 'api/update_password', :to=>'api#update_password',via: [:get,:post]
-
-  get 'api/my_journey', :to=>'api#my_journey'
-
-  get 'api/contents_list', :to=>'api#contents_list'
-
-  get 'api/contents_essence_list', :to=>'api#contents_essence_list'
-
-  get 'api/contents_cross_list', :to=>'api#contents_cross_list'
-
-  get 'api/content_comments_list', :to=>'api#content_comments_list'
-
-  get 'api/my_collects_list', :to=>'api#my_collects_list'
-
-  get 'api/collect_content', :to=>'api#collect_content'
-  get 'api/uncollect_content', :to=>'api#uncollect_content'
-
-  get 'api/praise_content', :to=>'api#praise_content'
-
-  match 'api/submit_comment', :to=>'api#submit_comment', via: [:get, :post]
-
-  match 'api/feedbacks', :to=>'api#feedbacks', via: [:get, :post]
-
-  post 'api/upload_avatar', :to=>'api#upload_avatar'
-
-
-  ##############################
-  get 'sessions/logout', :to=>'sessions#logout'
-  resources :sessions
-
-  get 'contents/delete_record', :to=>'contents#delete_record'
-  get 'contents/test_qiniu', :to=>'contents#test_qiniu'
-  post 'contents/upload_pic', :to=>'contents#upload_pic'
-  post 'contents/batch_update', :to=>'contents#batch_update'
-  resources :contents
-
-  get 'sensitive_words/delete_record', :to=>'sensitive_words#delete_record'
-  resources :sensitive_words
-
-  resources :users
-  resources :feedbacks
-
-  ############################################
-  ########### finance api
-  get 'api/finance_news_list', :to=>'api#finance_news_list'
-  get 'api/finance_info', :to=>'api#finance_info'
 
   ############################################
   ########### finance managers
@@ -84,53 +24,20 @@ FinanceApp::Application.routes.draw do
 
   resources :product_histories
 
+  ##############################
+  get 'sessions/logout', :to=>'sessions#logout'
+  resources :sessions
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'sensitive_words/delete_record', :to=>'sensitive_words#delete_record'
+  resources :sensitive_words
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  resources :users
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  ############################################
+  ########### finance api
+  namespace :api, :controller=>'api' do
+    match 'finance_news_list', :to=>'/api#finance_news_list', via: [:get, :post]
+    match 'finance_info', :to=>'/api#finance_info', via: [:get, :post]
+  end
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
